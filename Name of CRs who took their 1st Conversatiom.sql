1da6a31e621d93daea31f618c808ca38ec87166f
Eyes Well <chenerjun@gmail.com>
2019-02-20 09:40:28 -0500

    Delete Name of CRs who took their 1st Conversatiom.sql

--- Name of CRs who took their 1st Conversatiom.sql
+++
@@ -1,8 +0,0 @@
-select 
- u.firstname+'_'+u.lastname as name
-,  u.email
-, min([g.startTime:est:date]) as FirstConversation
-from [general_sigtables as g]  join user as u on g.user_ID = u. ID
-where u.email not like '%@crisistextline.org%'
-group by name, u.email
-order by name
